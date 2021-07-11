92fdb8fdace0bf37e1e05f0acc78fd9eafa2c16a
Simon Reber <sr.dev@outlook.com>
2021-07-11 23:07:58 +0200

    first commit

---
+++ desktop.cmd
@@ -0,0 +1,35 @@
+@echo off
+cd /d "%~dp0"
+for %%i in (.) do set fn=%%~nxi
+
+attrib . | find "R"
+if errorlevel 1 goto:r+
+if errorlevel 0 goto:r-
+
+:r+
+echo +r
+if exist desktop.ini goto:r++
+echo [.ShellClassInfo] > desktop.ini
+echo ConfirmFileOp=0 >> desktop.ini
+echo IconFile=messages.ico >> desktop.ini
+echo IconIndex=0 >> desktop.ini
+echo InfoTip="%fn%" >> desktop.ini
+:r++
+attrib +r .
+attrib +h desktop.ini
+attrib +h %~nx0
+attrib +h messages.*
+goto:refresh
+
+:r-
+echo -r
+attrib -r .
+attrib -h -s desktop.ini
+attrib -h %~nx0
+attrib -h messages.*
+goto:refresh
+
+:refresh
+call ie4uinit -show
+@REM pause
+
