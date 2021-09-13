39526a9ed6eda2373fb6e72cb190320d6185ce54
Simon Reber <sr.dev@outlook.com>
2021-09-13 23:27:57 +0200

    more, better

--- wEBcMD/DocGenerate-CommandTypes-Diagrams.cmd
+++
@@ -1,4 +0,0 @@
-
-      
-call tplant --input ClientApp\src\api\FindAdressesBase.ts ClientApp\src\impl\FindAdresses.ts ClientApp\src\impl\CommandWrapper.ts --output Doc\ts\FindAdresses.puml -A
-      
