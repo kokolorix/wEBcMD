34614865d2b4f8b6a982724050585d88044d01f3
Simon Reber <sr.dev@outlook.com>
2022-06-19 07:06:05 -0400

    New test for common things

---
+++ wEBcMDTests/Impl/CommonTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace wEBcMD.Tests
+{
+	[TestClass]
+	public class CommonTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			JsonSerializerOptions options = new JsonSerializerOptions();
+
+			ValueDTO val1 = ValueDTO.Create(42);
+
+			String json = JsonSerializer.Serialize(val1);
+
+			ValueDTO val2 = JsonSerializer.Deserialize<ValueDTO>(json);	
+			Console.WriteLine(JsonSerializer.Serialize(val2));
+
+
+
+			//options.SetupExtensions();
+			//DiscriminatorConventionRegistry registry = options.GetDiscriminatorConventionRegistry();
+			//registry.RegisterConvention(new AttributeBasedDiscriminatorConvention<string>(options, "Tag"));
+			//registry.RegisterType<Box>();
+			//registry.RegisterType<Circle>();
+
+			//Shape origin1 = new Box { Width = 10, Height = 20 };
+			//Shape origin2 = new Circle { Radius = 30 };
+
+			//string json1 = JsonSerializer.Serialize(origin1, options);
+			//string json2 = JsonSerializer.Serialize(origin2, options);
+
+			//Console.WriteLine(json1); // {"Tag":"Box","Width":10,"Height":20}
+			//Console.WriteLine(json2); // {"Tag":"Circle","Radius":30}
+
+			//var restored1 = JsonSerializer.Deserialize<Shape>(json1, options);
+			//var restored2 = JsonSerializer.Deserialize<Shape>(json2, options);
+
+			//Console.WriteLine(restored1); // Box: Width=10, Height=20
+			//Console.WriteLine(restored2); // Circle: Radius=30	}
+
+		}
+	}
+}
--- wEBcMDTests/Impl/SetAdressTests.cs
+++ wEBcMDTests/Impl/SetAdressTests.cs
@@ -8,94 +8,12 @@ using System.Threading.Tasks;
 
 namespace wEBcMD.Tests
 {
-    record ValueDTO
-    {
-		public static ValueDTO Create(Int32 v) => new ValueDTO.NumberValueDTO.Int32ValueDTO(v);
-		public static ValueDTO Create(Int64 v) => new ValueDTO.NumberValueDTO.Int64ValueDTO(v);
-        public static ValueDTO Create(Double v) => new ValueDTO.NumberValueDTO.FloatValueDTO(v);
-        public static ValueDTO Create(String v) => new ValueDTO.NumberValueDTO.StringValueDTO(v);
-        public static ValueDTO Create(Boolean v) => new ValueDTO.NumberValueDTO.BooleanValueDTO(v);
-        public static ValueDTO Create(BaseDTO v) => new ValueDTO.NumberValueDTO.ObjectValueDTO(v);
-
-        public String Str
-        {
-            get
-            {
-                switch (this)
-                {
-                    case NumberValueDTO.Int32ValueDTO:
-                        return ((NumberValueDTO.Int32ValueDTO)this).value.ToString();
-
-                    case NumberValueDTO.Int64ValueDTO:
-                        return ((NumberValueDTO.Int64ValueDTO)this).value.ToString();
-
-                    case NumberValueDTO.FloatValueDTO:
-                        return ((NumberValueDTO.FloatValueDTO)this).value.ToString();
-
-                    case NumberValueDTO.BooleanValueDTO:
-                        return ((NumberValueDTO.BooleanValueDTO)this).value.ToString();
-
-                    case NumberValueDTO.StringValueDTO:
-                        return ((NumberValueDTO.StringValueDTO)this).value.ToString();
-
-                    case NumberValueDTO.ObjectValueDTO:
-                        return ((NumberValueDTO.ObjectValueDTO)this).value.ToString();
-
-                    default:
-                        return String.Empty;
-                }
-            }
-        }
-
-        public record NumberValueDTO : ValueDTO
-        {
-			public NumberValueDTO() { }
-			public NumberValueDTO(Int64 i): this(){ }
-
-            public record Int32ValueDTO(Int32 value) : NumberValueDTO;
-             public record Int64ValueDTO(Int64 value) : NumberValueDTO;
-           public record FloatValueDTO(Double value) : NumberValueDTO;
-        };
-		public record BooleanValueDTO(Boolean value) : ValueDTO();
-		public record StringValueDTO(String value) : ValueDTO();
-		public record ObjectValueDTO(BaseDTO value) : ValueDTO();
-
-    }
-    //class NumberValueDTO : ValueDTO { }
-    //class BooleanValueDTO : ValueDTO { }
-    //class StringValueDTO : ValueDTO { }
-    //class ObjectValueDTO : ValueDTO { }
-
-	class ArgDTO
-    {
-		public String Name { get; set; }	
-
-		public ValueDTO Value { get; set; }
-
-    }
-
     [TestClass()]
 	public class SetAdressTests
 	{
 		[TestMethod()]
 		public void ExecuteCommandTest()
 		{
-			var v = ValueDTO.Create(2);
-			ArgDTO a = new() { Name = "Arg1" , Value = v };
-
-			String s = a.Value.Str;
-
-            List<ArgDTO> args = new()
-            {
-                new (){ Name= "Arg1", Value = ValueDTO.Create(7) },
-                new (){ Name= "Arg2", Value = ValueDTO.Create(3.14) },
-                new (){ Name= "Arg3", Value = ValueDTO.Create(true) },
-                new (){ Name= "Arg4", Value = ValueDTO.Create("Holdrio") },
-                new (){ Name= "Arg1", Value = ValueDTO.Create(
-                    new AdressDTO(){ Adress1 = "Street1", Name1= "Name1"}
-                    ) },
-            };
-
 			CommandDTO cmd2;
 			//{
 			SetAdressWrapper setAdress = new();
